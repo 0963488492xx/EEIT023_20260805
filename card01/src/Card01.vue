882a3c8950dc1d817ba04e4ea972e820464a7e9f
Ben Chen <idben.chen@gmail.com>
2026-08-05 15:58:21 +0800

    card01 專案: 建立樣式

--- card01/src/Card01.vue
+++ card01/src/Card01.vue
@@ -1,7 +1,32 @@
 <script setup></script>
 
 <template>
-    <h1>123</h1>
+    <div class="container py-3">
+        <div class="d-flex justify-content-center">
+            <div class="card w400px mh300px">
+                <img src="https://images.pexels.com/photos/13543169/pexels-photo-13543169.jpeg" alt="" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="fw-bold">轉讓交友特別是市場價，你們的出版，狀況。</h3>
+                    <p>
+                        正常看見裡面發出推薦使用語音很少簡體如今原本旁邊犯罪五年工資，裝修無派對付丈夫，斗內一人，時代一身。
+                    </p>
+                </div>
+                <div class="card-footer border-top-0 bg-white d-flex justify-content-between">
+                    <div class="btn btn-primary">上一個</div>
+                    <div class="btn btn-primary">下一個</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.w400px{
+    width: 400px;
+}
+.mh300px{
+    min-height: 300px;
+}
+</style>
