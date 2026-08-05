b7496d873a9cf3f813134ce937a3711bab179b03
Ben Chen <idben.chen@gmail.com>
2026-08-05 14:13:39 +0800

    test07 v-model 雙向綁定

---
+++ vue01/src/Test07.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+
+const userID = ref('123')
+const userPassword = ref('')
+
+const handleSubmit = ()=>{
+    console.log(userID.value, userPassword.value);
+    
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div class="row">
+      <label>id</label>
+      <input type="text" v-model="userID">
+    </div>
+    <div class="row">
+      <label>password</label>
+      <input type="password" v-model="userPassword">
+    </div>
+    <div class="ctrl">
+      <button>送出</button>
+    </div>
+  </form>
+</template>
+
+
+<style scoped>
+.row{
+    display: flex;
+    input{
+        flex: 1;
+    }
+}
+/* .row input{} */
+
+.ctrl{
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
