be1f67c2023e9140ff602340fb10ff037f393049
Ben Chen <idben.chen@gmail.com>
2026-08-05 10:45:51 +0800

    ref 的介紹

---
+++ vue01/src/Test01.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const message = ref('你好 Vue!')
+const name = ref('Ben Chen')
+const count = ref(0)
+let count2 = 0
+const isVisible = ref(true)
+const items = ref(['蘋果', '香蕉', '橘子'])
+
+count.value++
+count2++
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <h2>{{ name }}</h2>
+  <h3>{{ count }}</h3>
+  <h3>{{ isVisible }}</h3>
+  <h3>{{ items[count] }}</h3>
+  <h3>{{ items[count2] }}</h3>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b883;
+}
+</style>
