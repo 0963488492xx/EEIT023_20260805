9f2ef2e7ffa07600f6dc4b3d1a5d59da766e1e5a
Ben Chen <idben.chen@gmail.com>
2026-08-05 13:37:12 +0800

    test05 v-on

---
+++ vue01/src/Test05.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue'
+
+const count = ref(0)
+let count2 = 0
+
+const handleClick = () => {
+    count.value++
+}
+
+const handleClick2 = () => {
+    count2++
+}
+
+function handleClick3(num){
+    count.value+=num
+}
+</script>
+
+<template>
+    <h1>{{ count }}</h1>
+    <button v-on:click="handleClick">點擊</button>
+    <button @click="count++">直接增加</button>
+    <button @click="handleClick3(3)">傳參數</button>
+</template>
+
+<style scoped></style>
