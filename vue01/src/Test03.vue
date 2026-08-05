3612d74b129ebb37300275478aee740a16a3c255
Ben Chen <idben.chen@gmail.com>
2026-08-05 11:34:15 +0800

    test03 文字插值

---
+++ vue01/src/Test03.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref, reactive} from 'vue'
+const message = 'Hello, Vue!'
+const isActive = true
+</script>
+
+<template>
+    <!-- {{  }} 只能使用在元素當中， 不能使用在元素的屬性裡 -->
+    <!-- <img src="" alt=""> src, alt 就是元素的屬性 -->
+    <p>訊息: {{ message }}</p>
+    <p>計算: {{ 1+1 }}</p>
+    <p>呼叫方法: {{ message.toUpperCase() }}</p>
+    <p>三元運算子: {{ isActive ? '啟用' : '停用' }}</p>
+</template>
+
+<style scoped></style>
