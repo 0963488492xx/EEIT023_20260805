632c47e3812c0798504cc339e4a6abf75a5d7268
Ben Chen <idben.chen@gmail.com>
2026-08-05 13:58:13 +0800

    test06 事件修飾詞

---
+++ vue01/src/Test06.vue
@@ -0,0 +1,27 @@
+<script setup>
+const handleSubmit = function(event){
+    event.preventDefault()
+}
+
+const handleSubmit2 = () => {
+    console.log("wwwww");
+}
+
+function handleEnter(){
+    console.log("按了一下 ENTER");
+    
+}
+</script>
+
+<template>
+    <!-- <form action="" @submit="handleSubmit"> -->
+    <form action="" @submit.prevent.once="handleSubmit2">
+        <label>姓名</label>
+        <input type="text" name="name"><br>
+        
+        <button>送出</button>
+    </form>
+    <input type="text" @keyup.enter="handleEnter"><br>
+</template>
+
+<style scoped></style>
